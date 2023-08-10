1de438b9e1661912df9e48f6bd9b02418a0ee21a
vismo_dev <vismodeb2000@gmail.com>
2023-08-10 17:46:41 +0600

    PSMS Admin Subject All View Done

--- header.php
+++ header.php
@@ -231,7 +231,7 @@
             </a>
             <div class="collapse" id="ui-4">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="">All Subject</a></li>
+                <li class="nav-item"> <a class="nav-link" href="subject_all.php">All Subject</a></li>
                 <li class="nav-item"> <a class="nav-link" href="subject_add.php">Add New</a></li>
               </ul>
             </div>
---
+++ subject_all.php
@@ -0,0 +1,66 @@
+<?php require_once('header.php'); ?>
+
+    <div class="page-header">
+        <h3 class="page-title">
+            <span class="page-title-icon bg-gradient-primary text-white mr-2">
+            <i class="mdi mdi-account-multiple"></i>                 
+            </span>
+            All Subject
+        </h3>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Basic elements</li>
+            </ol>
+        </nav>
+    </div>
+
+    <div class="col-md-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+
+            <div class="form-group row">
+                <div class="col-sm-7 ml-auto">
+                    <h3>All Subject</h3>
+                </div>
+            </div>
+
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th><b>#</b></th>
+                            <th><b>Subject Name</b></th>
+                            <th><b>Subject Code</b></th>
+                            <th><b>Subjcet Type</b></th>
+                            <th><b>Action</b></th>
+                        </tr>
+                    </thead>
+                    <thead>
+
+                    <?php
+                        $stm = $pdo->prepare("SELECT * FROM subject");
+                        $stm->execute();
+                        $subjectList = $stm->fetchAll(PDO::FETCH_ASSOC);
+                        $i=1;
+                        foreach($subjectList as $subject):
+                    ?>                       
+                        <tr>
+                            <th><?php echo $i; $i++;?></th>
+                            <th><?php echo $subject['name'];?></th>
+                            <th><?php echo $subject['code'];?></th>
+                            <th><?php echo $subject['type'];?></th>
+                            <th>
+                                <a href="" class="btn btn-sm btn-gradient-dark"><i class="mdi mdi-table-edit"></i></a>
+                                <a href="" class="btn btn-sm btn-danger"><i class="mdi mdi-delete-forever"></i></a>
+                            </th>
+                        </tr>
+                    <?php endforeach;?>
+
+                    </thead>
+                </table>
+
+            </div>
+        </div>
+    </div>
+
+<?php require_once('footer.php'); ?>
